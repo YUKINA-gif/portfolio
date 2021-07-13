1ffadbe01a2236a340c12036b9d459b16bb43ff6
Yukina Nakanishi <yukina-nakanishi@outlook.jp>
2021-07-13 23:20:24 +0900

    CSS修正

---
+++ src/components/AboutMe.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="aboutme">
+    <h2 class="title">About me</h2>
+    <div class="flex">
+      <div class="aboutme_image">
+        <div class="image_back"></div>
+      </div>
+      <div class="aboutme_text">
+        <p>中西 由季奈</p>
+        <p>1993年生まれ</p>
+        <p>
+          ECサイト運営の受注としてお客様対応する中でサイトについてのエラーや問い合わせを答えられないことにもどかしく感じ、プログラミングの勉強をはじめる。それがとても楽しく、自分にはモノづくりがあっていることに気付き、1月下旬から本格的に勉強を開始。
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+/* =====================
+      About me
+====================== */
+#aboutme {
+  width: 100%;
+  margin: 0 auto;
+}
+.aboutme_image {
+  position: relative;
+  width: 350px;
+  height: 320px;
+  border-radius: 10%;
+  background-image: url(https://yn-portfolio.s3.ap-northeast-3.amazonaws.com/profile.jpg);
+  background-size: cover;
+}
+.image_back {
+  width: 100%;
+  height: 320px;
+  position: absolute;
+  border-radius: 10%;
+  top: 20px;
+  left: 20px;
+  background-color: #f9efdc;
+  z-index: -10;
+}
+.aboutme_text {
+  width: 30%;
+  text-align: left;
+  font-size: 18px;
+  margin-left: 60px;
+}
+.flex {
+  justify-content: center;
+}
+
+/* ===================
+      レスポンシブ
+==================== */
+@media screen and (max-width: 768px) {
+  .aboutme,
+  .aboutme_image,
+  .aboutme_text {
+    width: 90%;
+    margin: 0 auto;
+  }
+  .flex {
+    flex-wrap: wrap;
+  }
+  .aboutme_text {
+    margin-top: 50px;
+  }
+}
+</style>
---
+++ src/components/Contact.vue
@@ -0,0 +1,187 @@
+<template>
+  <div class="contact">
+    <h2 class="title">Contact</h2>
+    <ValidationObserver ref="observer">
+      <form action="">
+        <ul class="contact_form">
+          <li>
+            <ValidationProvider
+              rules="required"
+              ref="myname"
+              v-slot="{ errors }"
+            >
+              <label for="name">
+                <font-awesome-icon icon="user" class="icon" />
+              </label>
+              <input
+                type="text"
+                id="name"
+                placeholder="Name"
+                name="Name"
+                v-model="name"
+              /><br />
+              <span class="error">{{ errors[0] }}</span>
+            </ValidationProvider>
+          </li>
+          <li>
+            <ValidationProvider
+              rules="required|email"
+              ref="myname"
+              v-slot="{ errors }"
+            >
+              <label for="email">
+                <font-awesome-icon icon="envelope" class="icon" />
+              </label>
+              <input
+                type="text"
+                id="email"
+                name="Email"
+                placeholder="Email"
+                v-model="email"
+              /><br />
+              <span class="error">{{ errors[0] }}</span>
+            </ValidationProvider>
+          </li>
+          <li>
+            <ValidationProvider
+              rules="required"
+              ref="myname"
+              v-slot="{ errors }"
+            >
+              <label for="text">
+                <font-awesome-icon icon="pen" class="icon" />
+              </label>
+              <textarea
+                name="Text"
+                id="text"
+                cols="40"
+                rows="8"
+                placeholder="Message"
+                v-model="text"
+              ></textarea
+              ><br />
+              <span class="error">{{ errors[0] }}</span>
+            </ValidationProvider>
+          </li>
+        </ul>
+        <button class="button" type="button" @click="contact">
+          Send
+        </button>
+      </form>
+    </ValidationObserver>
+  </div>
+</template>
+
+<script>
+import {
+  ValidationObserver,
+  ValidationProvider,
+  extend,
+  localize,
+} from "vee-validate";
+import ja from "vee-validate/dist/locale/ja";
+import * as rules from "vee-validate/dist/rules";
+import axios from "axios";
+for (let rule in rules) {
+  extend(rule, rules[rule]);
+}
+localize("ja", ja);
+export default {
+  data() {
+    return {
+      name: "",
+      email: "",
+      text: "",
+    };
+  },
+  components: {
+    ValidationObserver,
+    ValidationProvider,
+  },
+  methods: {
+    contact() {
+      // バリデーション
+      this.$refs.observer.validate().then((res) => {
+        // バリデーションが有効ならスルー
+        if (res == false) {
+          console.log(res);
+          // そうでなければ送信
+        } else {
+          axios
+            .post("https://yukinas-portfolio.herokuapp.com/api/contact", {
+              name: this.name,
+              email: this.email,
+              text: this.text,
+            })
+            .then((res) => {
+              alert("メッセージの送信が完了しました。");
+              console.log(res);
+              this.$router.go({
+                path: this.$router.currentRoute.path,
+                force: true,
+              });
+            })
+            .catch((err) => {
+              alert(
+                "メッセージの送信ができませんでした。お手数ですが、再度お試しください。"
+              );
+              console.log(err);
+            });
+        }
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* =====================
+      Contact
+====================== */
+form {
+  width: 70%;
+  margin: 0 auto;
+}
+input,
+textarea {
+  width: 85%;
+  padding: 10px;
+  border: none;
+  outline: none;
+  border: 1px dashed #907b62;
+}
+textarea {
+  resize: none;
+  vertical-align: top;
+}
+.icon {
+  color: #907b62;
+  width: 25px;
+  height: 20px;
+  margin-right: 5px;
+}
+.button {
+  color: #fff;
+  background-color: #907b62;
+  border: 1px solid #907b62;
+  cursor: pointer;
+  padding: 10px 15px;
+  margin-bottom: 20px;
+}
+.error {
+  color: red;
+  margin-left: 30px;
+}
+/* ===================
+      レスポンシブ
+==================== */
+@media screen and (max-width: 768px) {
+  .contact_form li {
+    width: 98%;
+  }
+  form {
+    margin: 0 auto;
+    width: 100%;
+  }
+}
+</style>
